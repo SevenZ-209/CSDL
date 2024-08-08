483d46fb69fb5ad77b91ed0a71b61778bef487d4
SevenZ-209 <lekhoa5263@gmail.com>
2024-08-08 11:33:26 +0700

    Add files via upload

---
+++ SQLQuery15.sql
@@ -0,0 +1,13 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [CustomerID]
+      ,[CompanyName]
+      ,[ContactName]
+      ,[ContactTitle]
+      ,[Address]
+      ,[City]
+      ,[Region]
+      ,[PostalCode]
+      ,[Country]
+      ,[Phone]
+      ,[Fax]
+  FROM [Northwind].[dbo].[Customers1]
