11250968a9d858ca605a6b0e800d497c46f55a6a
SevenZ-209 <lekhoa5263@gmail.com>
2024-09-04 01:07:15 +0700

    Update QLSACH.sql

--- QLSACH.sql
+++ QLSACH.sql
@@ -1,43 +1,44 @@
-use za;
+use master;
+go
 
-CREATE TABLE BANGSACH
-(
-	MaSach nvarchar(10) not null primary key,
-	TenSach nvarchar(50) null,
-	TacGia nvarchar(50) null,
-	TenNhaXuatBang nvarchar(50) null,
-	GiaMua money null,
-	GhiChu nvarchar(80) null,
-);
+create database QLThueSach;
+go
 
-CREATE TABLE LOAISACH
-(
-	MaLoaiSach nvarchar(10) not null primary key,
-	TenLoaiSach nvarchar(100) null,
+use QLThueSach;
+go
+create table Sach
+	(
+		MaSach nvarchar(10) not null primary key,
+		TenSach nvarchar(50) null,
+		TacGia nvarchar(50) null,
+		TenNhaXuatBan nvarchar(100) null,
+		GiaMua money null,
+		GhiChu nvarchar(100) null,
+	);
+
+create table LoaiSach(
+	LoaiSach nvarchar(10) not null primary key,
+	TenLoaiSach nvarchar(10) null,
 	MieuTaLoaiSach nvarchar(255) null,
-
 );
 
-CREATE TABLE KHACHHANG
-(
+create table KhachHang(
 	MaKhachHang int not null primary key,
 	TenKhachHang nvarchar(50) null,
 	DiaChi nvarchar(100) null,
-	SoDienThoai nvarchar(12) null,
-	LoaiSachYeuThich nvarchar(100) null,
-	GhiChu nvarchar null,
+	SoDienThoai nvarchar(12),
+	LoaiSachYeuThich Nvarchar(100),
+	GhiChu nvarchar(MAX),
 
 );
 
-CREATE TABLE THUESACH
-(
+create table ThueSach(
 	MaKhachHang int not null,
 	MaSach nvarchar(10) not null,
-	NgayMuon Date not null,
-	NgayTra Date null,
+	NgayGioMuon date not null,
+	NgayGioTra date null,
 	SoTienThue money null,
-	constraint pk_thuesach primary key (MaKhachHang , MaSach, NgayMuon),
-	constraint fk_thuesach_khach_hang foreign key (MaKhachHang) references KHACHHANG (MaKhachHang),
-	constraint fk_thuesach_ma_sach foreign key (MaSach) references BangSach(MaSach),
-	
+	constraint PK_ThueSach Primary key (MaKhachHang, MaSach,NgayGioMuon),
+	constraint FK_MaKhachHang Foreign Key (MaKhachHang) references KhachHang(MaKhachHang),
+	constraint FK_MaSach Foreign Key (MaSach) references Sach(MaSach),
 );
